7f1e765bf36d54813fa6607c6fd7cdfe3eb02522
Z2r0-ux <2625492669@qq.com>
2026-09-22 18:47:37 +0800

    Configure workout history table

--- data/supabase-config.js
+++ data/supabase-config.js
@@ -1,5 +1,6 @@
 window.SUPABASE_CONFIG = {
   url: "https://cthooglrrlioiafyefnf.supabase.co",
   publishableKey: "sb_publishable_TSF446XfIxlyfSnDmMAxLw_dAqHsxwT",
-  table: "user_state"
+  table: "user_state",
+  historyTable: "workout_history"
 };
